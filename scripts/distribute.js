253117dd0bf38a246963fc5ff13638423c1e4fb5
Jaival Patel <pateljaival05@gmail.com>
2022-06-25 21:59:03 -0400

    single sending

--- scripts/distribute.js
+++ scripts/distribute.js
@@ -1,10 +1,10 @@
-import Web3 from 'web3'           
+const Web3 = require('web3')         
 const web3 = new Web3(Web3.givenProvider || 'https://eth-goerli.gateway.pokt.network/v1/lb/62b7830e123e6f003984c794') // Pokt Integration
-import promptSync from 'prompt-sync';
-const prompt = promptSync()
+const prompt = require('prompt-sync')()
 
-import EpnsSDK from '@epnsproject/backend-sdk-staging';
-import { api, utils } from "epnsproject/frontend-sdk-staging"; 
+const EpnsSDK = require("@epnsproject/backend-sdk-staging").default;
+const ethers = require('ethers');
+// import { api, utils } from "@epnsproject/frontend-sdk-staging"; 
 
 const connection_status = () => {
     const result = web3.version
@@ -12,44 +12,35 @@ const connection_status = () => {
 }
 
 
-/* async function create_transaction (){
-    const tx1 = await web3.eth.sendTransaction({
-    from: '0xec2a636B21E2935897DB0D779A66221A82B4fd02',
-    to:'0x127a95027B5c7E1D807433837C9cDD7e6f336803',
-    value: '50000000'
-    })
-
-    console.log(tx1)
-} */
 
 async function query_chain () {
-    let rel_contract = undefined
+    
     try {
         let fetch_version = connection_status()
         const given_contract = prompt('Contract Address to Query: ')
-        rel_contract = given_contract
         let contract_check = web3.utils.isAddress(given_contract)
         if (contract_check === false) {
             console.log(`${given_contract} was not a valid contract`)
         } else {
             // await create_transaction()
-            /*let real_addresses = web3.eth.getPastLogs(given_contract)
+            /*
+            let real_addresses = web3.eth.getPastLogs(given_contract)
             if (real_addresses.length === 0) {
                 console.log('There was an error with the contract')
             } else {
                 
                 let real_addresses_idx = 0 
-                holder_addresses = []
+                let holder_addresses = []
                 for (real_addresses_idx; real_addresses_idx <= real_addresses.length; real_addresse_idx++) {
-                    trans_dict = real_addresses[real_addresses_idx]
+                    const trans_dict = real_addresses[real_addresses_idx]
                     holder_address = trans_dict['address']
-                    holder_addresses.push(holder_addresses)
+                    holder_addresses.push(holder_address)
                 }
                 
-            }
-            */
+            }*/
+        
 
-            const sample_addresses = ['0x127a95027B5c7E1D807433837C9cDD7e6f336803', '0x2b02CdeD4C5Cc09455A4630d95556025CBe8991d']
+            const sample_addresses = ['0x52fA3cD7C8926CF515a454658A27d710CF447b2f' ,'0x127a95027B5c7E1D807433837C9cDD7e6f336803']
                         // Sample Addresses were needed to be made due to no smart contracts being on Goerli
             // The Conditional above would not work
             while (true) {
@@ -73,7 +64,7 @@ async function query_chain () {
 
     //}
     } catch (err) {
-        console.log(`${rel_contract} is not a valid address`)
+        console.log(`Contract is not a valid address`)
         console.log(err)
     }
     
@@ -105,18 +96,15 @@ async function fetch_notifications (user_address) {
 
 async function broadcast_message (addresses) {
     const CHANNEL_PK = process.env.PRIVATE_KEY
-    const epnsSDK = new EpnsSDK (
-        {
-            channelKey: process.env.PRIVATE_KEY,
-            channelAddress: process.env.WALLET_ADDRESS,
-            notificationChainId: DEFAULT_NOTIFICATION_CHAIN
-    })
-    message_title = prompt('Title of Message: ')
-    message_data = prompt('Message you want to Broadcast: ')
+    const epnsSDK = new EpnsSDK('0x5a5d6c906d34abb44538ede840ad3557911813e09944b01b10dab4a8e4cfa03f')
+    const message_title = prompt('Title of Message: ')
+    const message_data = prompt('Message you want to Broadcast: ')
+    
     
     let address_idx = 0
     for (address_idx; address_idx <= addresses.length; address_idx++) {
         current_address = addresses[address_idx]
+        console.log(current_address)
         
         const tx = await epnsSDK.sendNotification(
             current_address,
